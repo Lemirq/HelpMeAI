87b92d1b4251e339e19fe2c6f11b4cf60f5ae961
polyhistor2050 <chrisspaul2050@gmail.com>
2024-08-10 00:01:54 +0300

    Update the sidebar icons

--- app/sidebar/page.tsx
+++ app/sidebar/page.tsx
@@ -1,3 +1,7 @@
+import { MdDashboard } from "react-icons/md";
+import { FaInbox } from "react-icons/fa6";
+import { IoMdText } from "react-icons/io";
+import { MdContactSupport } from "react-icons/md";
 
 export default function Sidebar () {
     return (
@@ -6,27 +10,31 @@ export default function Sidebar () {
                 id="sidebar"
                 className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
                 <div className="h-full px-3 py-4 overflow-y-auto bg-indigo-200 dark:bg-gray-800">
-                            <h2 href="#" className="flex items-center p-2 text-indigo-900 font-bold mb-4 font rounded-lg dark:text-white dark:hover:bg-gray-700 group">
-                                <span className="ms-3 text-lg"> HelpMe AI </span>
-                            </h2>
+                    <h2 href="#" className="flex items-center p-2 text-indigo-900 font-bold mb-4 font rounded-lg dark:text-white dark:hover:bg-gray-700 group">
+                        <span className="ms-3 text-lg"> HelpMe AI </span>
+                    </h2>
                     <ul className="space-y-2 font-medium">
                         <li>
                             <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <MdDashboard />
                                 <span className="ms-3"> Overview </span>
                             </a>
                         </li>
                         <li>
                             <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <FaInbox />
                                 <span className="flex-1 ms-3 whitespace-nowrap">Inbox</span>
                             </a>
                         </li>
                         <li>
                             <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <IoMdText />
                                 <span className="flex-1 ms-3 whitespace-nowrap">Charts</span>
                             </a>
                         </li>
                         <li>
                             <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <MdContactSupport />
                                 <span className="flex-1 ms-3 whitespace-nowrap">Support</span>
                             </a>
                         </li>

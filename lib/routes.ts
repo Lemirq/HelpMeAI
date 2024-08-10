d565c11020307379809174ab7996cd3db9b8c3f8
colspan <collin@collindigm.com>
2024-08-10 14:08:41 +0300

    Routes type defined for public and protected routes

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,11 +1,3 @@
-/**
- * This setup does a few things:
- *
- * We define an AppRoutes object with our routes as constant strings.
- * We use as const to make TypeScript infer the most specific type possible.
- * We create a HelpMeAIRoute type that represents any of the values in AppRoutes.
- */
-
 export const AppRoutes = {
     HOME: '/',
     LOGIN: '/login',
@@ -15,3 +7,9 @@ export const AppRoutes = {
 } as const;
 
 export type HelpMeAIRoute = typeof AppRoutes[keyof typeof AppRoutes];
+
+export const ProtectedRoutes: HelpMeAIRoute[] = [
+    AppRoutes.DASHBOARD,
+    AppRoutes.HISTORY,
+    AppRoutes.TICKET,
+];

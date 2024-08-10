bb62d91121a4ee9233272b94fd47f9fadb4e5819
colspan <collin@collindigm.com>
2024-08-10 13:21:37 +0300

    Home/Landing, user auth-form and page clean-up

--- app/login/user-auth-form.tsx
+++ app/login/user-auth-form.tsx
@@ -3,9 +3,7 @@
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
-import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import { login } from './actions';
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}

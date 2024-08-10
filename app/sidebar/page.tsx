cb3c316d878e37c7721eb5b8dd0f6585be7558cd
polyhistor2050 <chrisspaul2050@gmail.com>
2024-08-10 15:28:25 +0300

    Updates

--- app/sidebar/page.tsx
+++ app/sidebar/page.tsx
@@ -57,8 +57,8 @@ export default function Sidebar() {
                     <div className="flex-grow overflow-y-auto px-3 scrollbar-hide">
                         <h3 className="text-lg font-semibold mb-2 sticky top-0 text-orange-500 bg-indigo-200 py-2"> Recent Charts: </h3>
                         {recentCharts.map((chart) => (
-                            <div key={chart.id} className="mb-2 hover:bg-gray-100 rounded-lg p-2">
-                                <h4 className="font-medium"> {chart.title} </h4>
+                            <div key={chart.id} className="mb-2 hover:bg-gray-100 hover:cursor-pointer rounded-lg p-2">
+                                <a href="#" className="font-medium hover:cursor-pointer"> {chart.title} </a>
                                 <p className="text-sm text-gray-600"> {chart.description} </p>
                             </div>
                         ))}
